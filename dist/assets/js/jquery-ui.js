25abc432d2521e1049f631695786bad9a7bb3b17
koo9ku <koo9ku@gmail.com>
2019-05-08 14:52:56 +0900

    racoon

--- dist/assets/js/jquery-ui.js
+++ dist/assets/js/jquery-ui.js
@@ -16758,7 +16758,7 @@
     dayNamesMin: ['일', '월', '화', '수', '목', '금', '토'],
     weekHeader: '주',
     // dateFormat: 'yymmdd',
-    firstDay: 1,
+    firstDay: 0,
     isRTL: false,
     showMonthAfterYear: true,
     yearSuffix: '년'
